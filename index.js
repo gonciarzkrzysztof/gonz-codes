52c370297174f9de691415bb6ac115a437f1bf40
Krzysztof Gonciarz <gonciarz.krzysztof@gmail.com>
2021-04-27 00:05:40 +0200

    changelog added

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello world')
+const changelog = document.querySelector('[data-changelog]');
+const changelogList = changelog.querySelector('[data-changelog-list]');
+
+async function fetchCommits() {
+  const response = await fetch('https://api.github.com/repos/gonciarzkrzysztof/gonz-codes/commits');
+  const commits = response.json();
+  return commits;
+}
+
+async function populateCommits() {
+  const referenceNode = changelogList.querySelector('[data-changelog-item]');
+
+  fetchCommits().then(commits => {
+    commits.map(commit => {
+      const clone = referenceNode.cloneNode(true);
+      referenceNode.before(clone);
+      const message = clone.querySelector('[data-changelog-message]');
+      const date = clone.querySelector('[data-changelog-date]');
+
+      message.innerText = commit.commit.message;
+      message.href = commit.html_url;
+      date.innerText = commit.commit.author.date.split(/[A-Za-z]/g).filter(Boolean).join(', ');
+      date.dateTime = commit.commit.author.date;
+    })
+
+    referenceNode.remove();
+  })
+}
+
+populateCommits()
